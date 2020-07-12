77367f7e56ca9d8dbe4fce2b1f257ccdf5d0031a
Necmettin Çimen <mail@necmettin.me>
2020-07-13 00:01:07 +0300

    first

---
+++ backend/NecSys.Api/Controllers/ModulController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace NecSys.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ModulController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ResultDto> Post([FromBody] PostDto values)
+        {
+            try
+            {
+
+                var item = JsonSerializer.Deserialize<Modul>(values.values);
+
+                if (!System.IO.Directory.Exists("Moduller"))
+                    System.IO.Directory.CreateDirectory("Moduller");
+
+                await System.IO.File.WriteAllTextAsync(Path.Combine("Moduller", item.ad + ".json"), values.values);
+
+                return new ResultDto(true);
+            }
+            catch (System.Exception ex)
+            {
+                return new ResultDto(ex.Message);
+            }
+        }
+        [HttpGet]
+        public ResultDto Get()
+        {
+            try
+            {
+                var files = Directory.GetFiles("Moduller");
+
+                return new ResultDto(files);
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+
+    public class PostDto
+    {
+        public string values { get; set; }
+    }
+
+    public class Modul
+    {
+        public string ad { get; set; }
+    }
+    public class ResultDto
+    {
+        public bool success { get; set; }
+        public dynamic result { get; set; }
+        public string error { get; set; }
+        public ResultDto(dynamic result)
+        {
+            this.result = result;
+            success = true;
+        }
+        public ResultDto(string error)
+        {
+            this.error = error;
+            success = false;
+        }
+    }
+
+}
---
+++ frontend/README.md
@@ -0,0 +1,16 @@
+# DevExtreme Angular Template
+
+The DevExtreme Angular Template is an Angular application with a navigation menu and sample views in a responsive layout (see a [live preview](https://devexpress.github.io/devextreme-angular-template)). This application is created with [Angular CLI](https://github.com/angular/angular-cli) and uses [DevExtreme Angular components](https://js.devexpress.com/Documentation/Guide/Angular_Components/DevExtreme_Angular_Components/).
+
+![DevExtreme-Angular-Template](https://user-images.githubusercontent.com/2280467/74529582-7bfb0500-4f39-11ea-830f-b8583c7a5478.png)
+
+## Getting Started
+
+For more information about the DevExtreme Angular Template and how to customize it, refer to the following help topic: [Create a DevExtreme Application](https://js.devexpress.com/Documentation/Guide/Angular_Components/Getting_Started/Create_a_DevExtreme_Application/).
+
+## License
+
+**DevExtreme Angular Template is released as a MIT-licensed (free and open-source) add-on to DevExtreme.**
+
+- [DevExtreme License](https://js.devexpress.com/Licensing/)
+- [Free trial](http://js.devexpress.com/Buy/)
